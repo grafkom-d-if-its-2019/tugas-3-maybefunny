d4c96002c897b23fde2e2785361f72cf0df49fe1
maybe <darfisultoni@gmail.com>
2019-10-30 17:27:19 +0700

    ok

--- vertex.glsl
+++ vertex.glsl
@@ -1,12 +1,14 @@
 precision mediump float;
 
-attribute vec2 vPosition;
+attribute vec3 vPosition;
 attribute vec3 vColor;
 varying vec3 fColor;
-uniform mat3 vMatrix;
+uniform mat4 vMatrix;
+uniform mat4 viewMatrix;
+uniform mat4 projectionMatrix;
 
 void main() {
   fColor = vColor;
 
-  gl_Position = vec4((vMatrix*vec3(vPosition, 1.0)).xy,0, 1.0);
+  gl_Position = projectionMatrix*viewMatrix*vMatrix*vec4(vPosition, 1.0);
 }
